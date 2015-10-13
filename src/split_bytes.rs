24417dce8b8433b52f3569d49c9e69b9c85a1c31
William Throwe <wtt6@cornell.edu>
2015-10-13 16:22:47 -0400

    Reorganize slightly and write Matches for bytes

--- src/split_bytes.rs
+++ src/split_bytes.rs
@@ -2,6 +2,35 @@ use core::str::pattern::{DoubleEndedSearcher, Pattern, ReverseSearcher, Searcher
 
 use utf8_sections::Utf8Sections;
 
+pub struct Matches<'a, P>(MatchImpl<'a, P>) where P: Pattern<'a>;
+
+impl<'a, P> Clone for Matches<'a, P>
+where P: Pattern<'a> + Clone, P::Searcher: Clone {
+    fn clone(&self) -> Self { Matches(self.0.clone()) }
+}
+
+impl<'a, P> Matches<'a, P> where P: Pattern<'a> {
+    pub fn new(slice: &'a [u8], pat: P) -> Self {
+        Matches(MatchImpl::new(slice, pat))
+    }
+}
+
+impl<'a, P> Iterator for Matches<'a, P> where P: Pattern<'a> + Clone {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<&'a str> {
+        self.0.next().map(|x| x.1)
+    }
+}
+
+impl<'a, P> DoubleEndedIterator for Matches<'a, P>
+where P: Pattern<'a> + Clone, P::Searcher: ReverseSearcher<'a> {
+    fn next_back(&mut self) -> Option<&'a str> {
+        self.0.next_back().map(|x| x.1)
+    }
+}
+
+
 pub struct Split<'a, P>(SplitImpl<'a, P>) where P: Pattern<'a>;
 
 impl<'a, P> Clone for Split<'a, P>
@@ -65,8 +94,8 @@ struct MatchImpl<'a, P> where P: Pattern<'a> {
     pat: P,
     front_searcher: Option<P::Searcher>,
     back_searcher: Option<P::Searcher>,
-    front_section_start: usize,
-    back_section_start: usize,
+    front_section: (usize, &'a str),
+    back_section: (usize, &'a str),
 }
 
 impl<'a, P> Clone for MatchImpl<'a, P>
@@ -77,8 +106,8 @@ where P: Pattern<'a> + Clone, P::Searcher: Clone {
             pat: self.pat.clone(),
             front_searcher: self.front_searcher.clone(),
             back_searcher: self.back_searcher.clone(),
-            front_section_start: self.front_section_start.clone(),
-            back_section_start: self.back_section_start.clone(),
+            front_section: self.front_section.clone(),
+            back_section: self.back_section.clone(),
         }
     }
 }
@@ -90,35 +119,36 @@ impl<'a, P> MatchImpl<'a, P> where P: Pattern<'a> {
             sections: Utf8Sections::new(slice),
             front_searcher: None,
             back_searcher: None,
-            front_section_start: 0,
-            back_section_start: slice.len(),
+            front_section: (0, ""),
+            back_section: (slice.len(), ""),
         }
     }
 
-    fn next(&mut self) -> Option<(usize, usize)> where P: Clone {
+    fn next(&mut self) -> Option<(usize, &'a str)> where P: Clone {
         loop {
-            if self.front_section_start == self.back_section_start {
+            if self.front_section.0 == self.back_section.0 {
                 // Last section.  Both directions use the same iterator.
                 let searcher = self.front_searcher.as_mut().or(self.back_searcher.as_mut());
-                let offset = self.front_section_start;
+                let section = self.front_section;
                 return searcher.and_then(|m| m.next_match())
-                    .map(|(start, end)| (offset + start, offset + end));
+                    .map(|(start, end)| (section.0 + start, &section.1[start..end]));
             }
             if self.front_searcher.is_none() {
-                if let Some((start, section)) = self.sections.next() {
+                if let Some(section) = self.sections.next() {
+                    self.front_section = section;
                     // New segment
-                    self.front_searcher = Some(self.pat.clone().into_searcher(section));
-                    self.front_section_start = start;
+                    self.front_searcher =
+                        Some(self.pat.clone().into_searcher(self.front_section.1));
                 } else {
                     // Starting the last segment.  Go back to the top.
-                    self.front_section_start = self.back_section_start;
+                    self.front_section = self.back_section;
                     continue;
                 }
             }
 
             if let Some((start, end)) = self.front_searcher.as_mut().unwrap().next_match() {
-                return Some((self.front_section_start + start,
-                             self.front_section_start + end));
+                let section = self.front_section;
+                return Some((section.0 + start, &section.1[start..end]));
             }
 
             // Finished this segment
@@ -126,31 +156,32 @@ impl<'a, P> MatchImpl<'a, P> where P: Pattern<'a> {
         }
     }
 
-    fn next_back(&mut self) -> Option<(usize, usize)>
+    fn next_back(&mut self) -> Option<(usize, &'a str)>
     where P: Clone, P::Searcher: ReverseSearcher<'a> {
         loop {
-            if self.front_section_start == self.back_section_start {
+            if self.front_section.0 == self.back_section.0 {
                 // Last section.  Both directions use the same iterator.
                 let searcher = self.front_searcher.as_mut().or(self.back_searcher.as_mut());
-                let offset = self.back_section_start;
+                let section = self.back_section;
                 return searcher.and_then(|m| m.next_match_back())
-                    .map(|(start, end)| (offset + start, offset + end));
+                    .map(|(start, end)| (section.0 + start, &section.1[start..end]));
             }
             if self.back_searcher.is_none() {
-                if let Some((start, section)) = self.sections.next_back() {
+                if let Some(section) = self.sections.next_back() {
                     // New segment
-                    self.back_searcher = Some(self.pat.clone().into_searcher(section));
-                    self.back_section_start = start;
+                    self.back_section = section;
+                    self.back_searcher =
+                        Some(self.pat.clone().into_searcher(self.back_section.1));
                 } else {
                     // Starting the last segment.  Go back to the top.
-                    self.back_section_start = self.front_section_start;
+                    self.back_section = self.front_section;
                     continue;
                 }
             }
 
             if let Some((start, end)) = self.back_searcher.as_mut().unwrap().next_match_back() {
-                return Some((self.back_section_start + start,
-                             self.back_section_start + end));
+                let section = self.back_section;
+                return Some((section.0 + start, &section.1[start..end]));
             }
 
             // Finished this segment
@@ -187,9 +218,9 @@ impl<'a, P> SplitImpl<'a, P> where P: Pattern<'a> {
 
     fn next(&mut self) -> Option<&'a [u8]> where P: Clone {
         if self.remainder.1 < self.remainder.0 { return None; }
-        if let Some((start, end)) = self.matches.next() {
+        if let Some((start, mat)) = self.matches.next() {
             let result = &self.slice[self.remainder.0..start];
-            self.remainder.0 = end;
+            self.remainder.0 = start + mat.len();
             Some(result)
         } else {
             // We've exhausted all the matches, but we still
@@ -204,8 +235,8 @@ impl<'a, P> SplitImpl<'a, P> where P: Pattern<'a> {
     fn next_back(&mut self) -> Option<&'a [u8]>
     where P: Clone, P::Searcher: ReverseSearcher<'a> {
         if self.remainder.1 < self.remainder.0 { return None; }
-        if let Some((start, end)) = self.matches.next_back() {
-            let result = &self.slice[end..self.remainder.1];
+        if let Some((start, mat)) = self.matches.next_back() {
+            let result = &self.slice[start + mat.len()..self.remainder.1];
             self.remainder.1 = start;
             Some(result)
         } else {
